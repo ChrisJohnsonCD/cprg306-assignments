e819a8793aca5af83aafff4a4538b4627f56882f
ChrisJohnsonCD <129870653+ChrisJohnsonCD@users.noreply.github.com>
2023-09-16 18:54:25 -0600

    week2
    week2 completed. Need feed back to see if it;s done right

---
+++ app/weeklink/week2.js
@@ -0,0 +1,12 @@
+import StudentInfo from "../StudentInfo"
+
+
+export default function Home() {
+    return (
+        <main>
+            <h1>My Shopping list</h1>
+            <StudentInfo />
+        </main>
+
+    )
+    }
