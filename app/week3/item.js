ebd0f3a2911e65a823be12243f01ea2accd9c136
ChrisJohnsonCD <129870653+ChrisJohnsonCD@users.noreply.github.com>
2023-09-29 10:38:02 -0600

    week3 minimal build- need to style

---
+++ app/week3/item.js
@@ -0,0 +1,12 @@
+
+
+export default function Item ({ name, quantity, category }) {
+    return (
+        <div className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2">
+            <h3 className="text-x1 font-bold">{name}</h3>
+            <p className="text-x1 font-bold"> {quantity}</p>
+            <p className="text-x1 font-bold"> {category}</p>
+        </div>
+        
+    );
+}
