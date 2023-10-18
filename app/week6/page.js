5c0a6229af9405921209c287c4b1bf01356d294c
ChrisJohnsonCD <129870653+ChrisJohnsonCD@users.noreply.github.com>
2023-10-18 10:48:06 -0600

    fixed all issues and got it working

--- app/week6/page.js
+++ app/week6/page.js
@@ -1,12 +1,29 @@
+"use client";
+
+import React, { useState } from 'react';
 import ItemList from './item-list';
+import NewItem from './new-item'; 
+import itemsData from './items.json';
 
 
-export default function Page() {
+
+
+export default function Page() 
+{ // Initializes the items state with the data from items.json
+  const[items, setItems] = useState (itemsData);
+
+  // Event handler to add new items to the items state
+  const handleAddItem = (newItem) => 
+  {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
     return (
       <main>
         <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
+
+        <NewItem onAddItem ={handleAddItem} />
       
-      <ItemList />
+      <ItemList items = {items}/>
       </main>
     );
   }
